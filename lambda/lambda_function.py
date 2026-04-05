c649a5dd75b089b41fd1364de48912f18d09b53e
Hasan Syed <hasansyed730@gmail.com>
2026-04-04 20:09:57 -0500

    Fixed issue with visitor counter

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,21 +1,41 @@
-# Lamdba function to write to DynamoDB for visitor count
 import json
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('VisitorCounter')
+table = dynamodb.Table('VisitorCount')
 
-#increment counter in dynamoDB table
 def lambda_handler(event, context):
-    response = table.update_item(
-        Key={'id': 'visitors'},
-        UpdateExpression='SET count' = if_not_exists(count, :start) + :inc',
-        ExpressionAttributeValues={
-            ':inc': 1,
-            ':start': 0
-        },
-        ReturnValues='UPDATED_NEW'
-    )
-    return 
-
-
+    try:
+        response = table.update_item(
+            Key={'id': 'visitors'},
+            UpdateExpression='SET count = if_not_exists(count, :start) + :inc',
+            ExpressionAttributeValues={
+                ':inc': 1,
+                ':start': 0
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        
+        # Extract the new count from the response
+        new_count = response['Attributes']['count']
+        
+        # Return success response to API Gateway
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'  # Allows your frontend to call it
+            },
+            'body': json.dumps({'count': new_count})
+        }
+        
+    except Exception as e:
+        # Return error response
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({'error': str(e)})
+        }
