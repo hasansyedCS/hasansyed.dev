193bc377f3a3365c6795ad47a03563863f966285
Hasan Syed <hasansyed730@gmail.com>
2026-04-04 20:37:21 -0500

    Fixed bug with lambda function. Working visitor counter

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,41 +1,60 @@
+# Visitor Counter Lambda for hasansyed.dev
+# Increments visitor count in DynamoDB and returns new count
 import json
 import boto3
+from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('VisitorCount')
+table = dynamodb.Table('VisitorCounter2.0')
 
 def lambda_handler(event, context):
     try:
         response = table.update_item(
             Key={'id': 'visitors'},
-            UpdateExpression='SET count = if_not_exists(count, :start) + :inc',
+            UpdateExpression='SET #vc = if_not_exists(#vc, :start) + :inc',
+            ExpressionAttributeNames={
+                '#vc': 'visitor_count'
+            },
             ExpressionAttributeValues={
                 ':inc': 1,
                 ':start': 0
             },
             ReturnValues='UPDATED_NEW'
         )
-        
-        # Extract the new count from the response
-        new_count = response['Attributes']['count']
-        
-        # Return success response to API Gateway
+
+        new_count = int(response['Attributes']['visitor_count'])
+
         return {
             'statusCode': 200,
             'headers': {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'  # Allows your frontend to call it
+                'Access-Control-Allow-Origin': '*'
             },
-            'body': json.dumps({'count': new_count})
+            'body': json.dumps({
+                'count': new_count
+            })
         }
-        
-    except Exception as e:
-        # Return error response
+
+    except ClientError as e:
         return {
             'statusCode': 500,
             'headers': {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*'
             },
-            'body': json.dumps({'error': str(e)})
+            'body': json.dumps({
+                'error': e.response['Error']['Message']
+            })
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({
+                'error': str(e)
+            })
         }
